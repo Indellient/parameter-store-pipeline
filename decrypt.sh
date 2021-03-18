cb4e307c348e7fad0480623605d1820778f086a6
Yonathan Koren <yonkornilov@live.com>
2021-03-18 14:25:16 -0400

    Make encryption and decryption scripts idempotent.

--- decrypt.sh
+++ decrypt.sh
@@ -3,5 +3,8 @@
 CONTEXT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 for file in $(find "$CONTEXT" -name "*.json");
 do
-    sops --decrypt -i ${file}
+    if [[ $(cat petshop-billing/dev/config.json | jq -r .sops) != null ]];
+    then
+        sops --decrypt -i ${file}
+    fi
 done
--- encrypt.sh
+++ encrypt.sh
@@ -3,5 +3,8 @@
 CONTEXT="$( cd -- "$(dirname "$0")" >/dev/null 2>&1 ; pwd -P )"
 for file in $(find "$CONTEXT" -name "*.json");
 do
-    sops --encrypt -i ${file}
+    if [[ $(cat petshop-billing/dev/config.json | jq -r .sops) == null ]];
+    then
+        sops --encrypt -i ${file}
+    fi
 done
