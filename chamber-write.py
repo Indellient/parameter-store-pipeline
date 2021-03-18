92e3c0d41e92c61a0305f543c9c225227aaff3e2
Yonathan Koren <yonkornilov@live.com>
2021-03-18 14:16:05 -0400

    Create Python script for updating AWS Parameter Store via Chamber.

---
+++ chamber-write.py
@@ -0,0 +1,12 @@
+import os
+
+service_config_locations = {
+    "petshop-billing"    : ["global", "dev", "qa", "uat", "prod"],
+    "petshop-inventory"  : ["global", "dev", "qa", "uat", "prod"],
+    "petshop-storefront" : ["global", "dev", "qa", "uat", "prod"]
+}
+
+for key in service_config_locations:
+    for env in service_config_locations[key]:
+        print(f"Writing config for {key}/{env}: {key}/{env}/config.json")
+        os.system(f"cat {key}/{env}/config.json | chamber write {key}/{env} config -")
